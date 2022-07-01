6aa7b7b65048c1e0c8b5c21d3e0d92cf5ea2dc7f
bogdanpashtet <ruslan.nnov@mail.ru>
2022-07-02 00:49:47 +0300

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='photos/%Y/%M/%D')
+    email = models.EmailField(unique=True, default="aaa@aa.com")
+    date_of_creation = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    role = models.IntegerField(default=0)
+
+
+class Articles(models.Model):
+    title = models.CharField(max_length=200, default="Enter the name please")
+    owner = models.CharField(max_length=50)
+    article_text = models.TextField(blank=True)
+    date_of_publishing = models.DateTimeField(auto_now_add=True)
+    # photo_preview = models.ImageField(upload_to='photos_article/%Y/%M/%D')
+    slug_name = models.SlugField(unique=True, max_length=200, default=0)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Articles
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+
+def profile(request):
+    return HttpResponse('<h1>This is a profile page</h1>')
